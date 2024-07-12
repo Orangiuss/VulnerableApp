d4d46977a11cacf916d80c9276a7f58334489ce2
Orangius <richarddufour@laposte.net>
2024-07-12 12:39:58 +0200

    V0.0

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # Set build type
-if [ "$1" == "rebuild" ]; then
+if [ "$1" == "-r" ]; then
   docker compose up -d --no-deps --build --remove-orphans
-elif [ "$1" == "down" ]; then
+  # Prepare the exploit
+  touch /tmp/pom.xml 
+  sleep 5
+  curl -v -u admin:admin123 --upload-file /tmp/pom.xml http://localhost:8081/repository/maven-releases/org/foo/1.0/foo-1.0.po > /dev/null 2>&1
+elif [ "$1" == "-d" ]; then
   docker compose down
+elif [ "$1" == "-h" ]; then
+  echo "Usage: setup.sh [option]"
+  echo "Options:"
+  echo "  -r  Rebuild the Docker containers"
+  echo "  -d  Stop and remove the Docker containers"
+  echo "  -h  Show this help message"
 else
   docker compose up -d
+  # Prepare the exploit
+  touch /tmp/pom.xml 
+  sleep 5
+  curl -v -u admin:admin123 --upload-file /tmp/pom.xml http://localhost:8081/repository/maven-releases/org/foo/1.0/foo-1.0.po > /dev/null 2>&1
 fi
-
-# Prepare the exploit
-#!/bin/bash
-touch /tmp/pom.xml 
-sleep 5
-curl -v -u admin:admin123 --upload-file /tmp/pom.xml http://localhost:8081/repository/maven-releases/org/foo/1.0/foo-1.0.po > /dev/null 2>&1
