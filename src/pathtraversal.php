5891bb0ca82ce1ffcc3b5ecd43afba2a31683295
Orangius <richarddufour@laposte.net>
2024-08-14 17:43:52 +0200

    Add pathtraversal exemple

---
+++ src/pathtraversal.php
@@ -0,0 +1,6 @@
+<?php
+header("Content-type: application/force-download");  
+header("Content-Disposition:attachment; filename=".$_GET["fichier"]);
+readfile($_GET["chemin"]."/".$_GET["fichier"]);
+unlink($_GET["chemin"]."/".$_GET["fichier"]);             
+?> 
